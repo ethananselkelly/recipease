16258d2c4672d3eb552efed055379854e37f0844
ethananselkelly <eanselke@gmail.com>
2022-08-07 12:23:07 -0400

    css

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -24,7 +24,7 @@ const TopBar = ({ user }) => {
     <div className="top-bar">
       <div className="top-bar-left">
         <ul className="menu">
-          <li className="menu-text">App</li>
+          <li className="menu-text">Recipease</li>
           <li>
             <Link to="/">Home</Link>
           </li>
