484dc130889039815be1f28e78238bc23615ccbf
ethananselkelly <eanselke@gmail.com>
2022-09-09 12:28:14 -0400

    home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -34,7 +34,7 @@ const Home = () => {
     <div className='index-container'>
       <div className='text-container'>
         <h2 className='header-text'>Recipease</h2>
-        <p>An easy to use web app for scraping and saving recipes. I made this app because I wanted a single place where I could find all of the recipes I've saved online in Youtube playlists, Chrome bookmarks, DMs, etc.</p>
+        <p>A web app for scraping and saving recipes. I made this app because I wanted a single place where I could find all of the recipes I've saved online in Youtube playlists, Chrome bookmarks, DMs, etc.</p>
         <p>A list of websites the app can scrape from can be found  <a href='https://github.com/hhursev/recipe-scrapers#scrapers-available-for' target='_blank'>here</a></p>
         <p>If you have questions, comments, or suggestions, you can reach me on LinkedIn or by email.</p>
         <p>Happy cooking 😋</p>
