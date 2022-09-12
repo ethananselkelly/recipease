2889fdc8c65665e8f3e17fa4437592c76d6f3e77
ethananselkelly <eanselke@gmail.com>
2022-09-12 13:24:37 -0400

    scraperchanges

--- server/src/db/migrations/20210101210207_createUsers.cjs
+++ server/src/db/migrations/20210101210207_createUsers.cjs
@@ -16,6 +16,7 @@ exports.up = async (knex) => {
     return knex.schema.createTable(tableName, (table) => {
       table.bigIncrements("id");
       table.string("email").notNullable().unique();
+      table.string('username').notNullable().unique();
       table.string("cryptedPassword").notNullable();
       table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
       table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
