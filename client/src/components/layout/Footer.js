e5e819c4a3a431e818402251592ca929eb6be79f
ethananselkelly <eanselke@gmail.com>
2022-09-20 21:36:12 -0400

    aboutme

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Divider from '@mui/material/Divider'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 const Footer = () => {
   return (
     <div className="fixed-bottom">
       <Divider />
       <div className="footer container">
-        <a className="link" href="https://github.com/ethananselkelly/recipease" target='_blank'>GitHub</a>
-        ||
-        <a className="link" href="https://www.linkedin.com/in/ethan-ansel-kelly/" target='_blank'>LinkedIn</a>
-        ||
-        <a className="link" href="mailto:eanselke@gmail.com">Email</a>
+        <a className="link" href="https://github.com/ethananselkelly/recipease" target='_blank'><GitHubIcon/></a>
+        |
+        <a className="link" href="https://www.linkedin.com/in/ethan-ansel-kelly/" target='_blank'><LinkedInIcon/></a>
+        |
+        <a className="link" href="mailto:eanselke@gmail.com"><EmailIcon/></a>
       </div>
     </div>
   )
