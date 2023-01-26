392db120fa5ab27e86e0f5bddfa5ef1024bd006f
ethananselkelly <eanselke@gmail.com>
2023-01-25 19:22:32 -0500

    added password reset columns to user table and updated user model

---
+++ server/src/db/migrations/20230125225125_addPasswordResetToUser.cjs
@@ -0,0 +1,23 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+    return knex.schema.table("users", (table) => {
+        table.string('passwordResetToken');
+        table.timestamp("passwordResetAt").notNullable().defaultTo(knex.fn.now());
+    })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+    return knex.schema.table("users", (table) => {
+        table.dropColumn("passwordResetToken")
+        table.dropColumn("passwordResetAt")
+    })
+}
