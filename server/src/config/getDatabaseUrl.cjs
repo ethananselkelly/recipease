cbf1195582bb92751922f9f437a358416ab0ef9f
ethananselkelly <eanselke@gmail.com>
2023-01-31 17:29:06 -0500

    yarnnn

--- server/src/config/getDatabaseUrl.cjs
+++ server/src/config/getDatabaseUrl.cjs
@@ -1,6 +1,7 @@
 const getDatabaseUrl = (nodeEnv) => {
   return (
     {
+      production: "postgres://postgresql_defined_69291_user:fYxU5HSaxkQCfi6kaszc82kLQbt7OZIW@dpg-ce52ndg2i3mjq96t7n6g-a/postgresql_defined_69291",
       development: "postgres://postgres:postgres@localhost:5432/recipease_development",
       test: "postgres://postgres:postgres@localhost:5432/recipease_test",
       e2e: "postgres://postgres:postgres@localhost:5432/recipease_e2e",
