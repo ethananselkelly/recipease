1c04be8c0b9620e55d53b26458a2a2c02a7ef3f6
ethananselkelly <eanselke@gmail.com>
2023-02-02 17:19:47 -0500

    dburl

--- server/src/config/getDatabaseUrl.cjs
+++ server/src/config/getDatabaseUrl.cjs
@@ -1,6 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 const getDatabaseUrl = (nodeEnv) => {
+  const productionDB = process.env.DATABASE_URL
   return (
     {
+      production: productionDB,
       development: "postgres://postgres:postgres@localhost:5432/recipease_development",
       test: "postgres://postgres:postgres@localhost:5432/recipease_test",
       e2e: "postgres://postgres:postgres@localhost:5432/recipease_e2e",
